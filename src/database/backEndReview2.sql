5973d76ca1b815702d825353dcd36247743e6a57
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-16 13:54:28 +0200

    creating users table

---
+++ src/database/backEndReview2.sql
@@ -0,0 +1,23 @@
+-- Active: 1686838426516@@127.0.0.1@3306
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL
+);
+
+INSERT INTO users (id, name, email, password, role)
+VALUES
+  -- tipo NORMAL e senha = fulano123
+	('u001', 'Fulano', 'fulano@email.com', '$2a$12$qPQj5Lm1dQK2auALLTC0dOWedtr/Th.aSFf3.pdK5jCmYelFrYadC', 'NORMAL'),
+
+  -- tipo NORMAL e senha = beltrana00
+	('u002', 'Beltrana', 'beltrana@email.com', '$2a$12$403HVkfVSUbDioyciv9IC.oBlgMqudbnQL8ubebJIXScNs8E3jYe2', 'NORMAL'),
+
+  -- tipo ADMIN e senha = astrodev99
+	('u003', 'Astrodev', 'astrodev@email.com', '$2a$12$lHyD.hKs3JDGu2nIbBrxYujrnfIX5RW5oq/B41HCKf7TSaq9RgqJ.', 'ADMIN');
+
+
+    SELECT * FROM users; 
