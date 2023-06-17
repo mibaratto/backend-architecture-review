2dd70c0a01d630cd7e7ae66482cc1add70fc7163
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-17 20:11:22 +0200

    user and playlist layers: business, controller and database

---
+++ src/controller/PlaylistController.ts
@@ -0,0 +1,8 @@
+import { PlaylistBusiness } from "../business/PlaylistBusiness";
+
+export class PlaylistController {
+    constructor(
+        private playlistBusiness: PlaylistBusiness
+    ){}
+    //endpoints
+}
---
+++ src/database/PlaylistDatabase.ts
@@ -0,0 +1,8 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class PlaylistDatabase extends BaseDatabase{
+    public static TABLE_PLAYLISTS = "playlists"
+    public static TABLE_LIKES_DISLIKES = "likes_dislikes"
+
+    //metodos DB
+}
