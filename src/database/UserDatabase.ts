703a96c94f26ff7c3cfb09a6ca696d8eafe767f5
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-18 12:32:56 +0200

    implementing user signup

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,5 +1,12 @@
+import { UserDB } from "../models/User";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
     public static TABLE_USERS = "users"
+
+    public insertUser = async (userDB: UserDB): Promise<void> => {
+        await BaseDatabase
+        .connection(UserDatabase.TABLE_USERS)
+        .insert(userDB)
+    }
 }
