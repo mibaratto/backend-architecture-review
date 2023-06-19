456b355d215217770adc118145d0cf178c68dbca
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-19 18:19:27 +0200

    playlist create PlaylistBusiness

--- src/business/PlaylistBusiness.ts
+++ src/business/PlaylistBusiness.ts
@@ -1,4 +1,8 @@
+import { createBrotliDecompress } from "zlib";
 import { PlaylistDatabase } from "../database/PlaylistDatabase";
+import { CreatePlaylistInputDTO, CreatePlaylistOutputDTO } from "../dtos/playlist/createPlaylist.dto";
+import { UnauthorizedError } from "../errors/UnauthorizedError";
+import { Playlist } from "../models/Playlist";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
 
@@ -8,5 +12,29 @@ export class PlaylistBusiness {
         private idGenerator: IdGenerator,
         private tokenManager: TokenManager
     ){}
-    //endpoints
+    public createPlaylist = async (
+        input: CreatePlaylistInputDTO
+    ): Promise<CreatePlaylistOutputDTO> => {
+        const { name, token} = input
+
+        const playload = this.tokenManager.getPayload(token)
+
+        if(!playload) {
+            throw new UnauthorizedError()
+        }
+
+        const id = this.idGenerator.generate()
+        const playlist = new Playlist(
+            id,
+            name,
+            0,
+            0,
+            new Date().toISOString(),
+            new Date().toISOString(),
+            playload.id,
+            playload.name
+        )
+        const playlistDB = playlist.toDBModel()
+        await this.playlistDatabse.insertPlaylist(playlistDB)
+    }
 }
