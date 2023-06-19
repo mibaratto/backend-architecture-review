411d9964cb75526fc61150dc71de948d2b3a0aec
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-19 15:31:44 +0200

    user login UserBusiness and UserDatabase

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -4,7 +4,9 @@ import { TokenManager } from "../services/TokenManager";
 import { UserDatabase } from "../database/UserDatabase";
 import { SignupInputDTO, SignupOutputDTO } from "../dtos/user/signup.dto";
 import { TokenPayload, USER_ROLES, User } from "../models/User";
-
+import { LoginInputDTO, LoginOutputDTO } from "../dtos/user/login.dto";
+import { NotFoundError } from "../errors/NotFoundError";
+import { BadRequestError } from "../errors/BadRequestError";
 
 export class UserBusiness{
     constructor (
@@ -43,6 +45,49 @@ export class UserBusiness{
         const output: SignupOutputDTO = {
             token
         }
+        return output
+    }
+
+    public login = async (
+        input: LoginInputDTO
+        ): Promise<LoginOutputDTO> => {
+        const { email, password } = input
+        
+        const userDB = await this.userDatabase.findUserByEmail(email)
+
+        if (!userDB) {
+            throw new NotFoundError("email not found")
+        }
+
+        const user = new User(
+            userDB.id,
+            userDB.name,
+            userDB.email,
+            userDB.password,
+            userDB.role,
+            userDB.created_at
+        )
+
+        const hashedPassword = user.getPassword()
+        
+        const isPasswordCorrect = await this.hashManager
+            .compare(password, hashedPassword)
+        
+        if (!isPasswordCorrect) {
+            throw new BadRequestError("invalid email or password")
+        }
+
+        const playload: TokenPayload = {
+            id: user.getId(),
+            name: user.getName(),
+            role: user.getRole()
+        }
+
+        const token = this.tokenManager.createToken(playload)
+        const output: LoginOutputDTO = {
+            token
+        }
+        
         return output
     }
 }
