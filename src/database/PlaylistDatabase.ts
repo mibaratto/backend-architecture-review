d54dd00dc8ad49e947f35ee4ed1f71bd48761e53
Michelle Lang Baratto <mi.baratto@gmail.com>
2023-06-19 18:25:26 +0200

    playlist create PlaylistBusiness

--- src/database/PlaylistDatabase.ts
+++ src/database/PlaylistDatabase.ts
@@ -1,8 +1,15 @@
+import { PlaylistDB } from "../models/Playlist";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class PlaylistDatabase extends BaseDatabase{
     public static TABLE_PLAYLISTS = "playlists"
     public static TABLE_LIKES_DISLIKES = "likes_dislikes"
 
-    //metodos DB
+    public insertPlaylist = async (
+        playlistDB: PlaylistDB
+    ): Promise<void> => {
+        await BaseDatabase
+        .connection(PlaylistDatabase.TABLE_PLAYLISTS)
+        .insert(playlistDB)
+    }
 }
